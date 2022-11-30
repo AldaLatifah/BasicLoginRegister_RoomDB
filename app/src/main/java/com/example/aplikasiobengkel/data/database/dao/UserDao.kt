25179dc9f7c4d1787b5615bcecc47e085e00345c
dionpouw <aldion.sumampouw@gmail.com>
2022-11-30 15:59:56 +0800

    change into suspend function

--- app/src/main/java/com/example/aplikasiobengkel/data/database/dao/UserDao.kt
+++ app/src/main/java/com/example/aplikasiobengkel/data/database/dao/UserDao.kt
@@ -1,15 +1,13 @@
 package com.example.aplikasiobengkel.data.database.dao
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.room.*
-import com.example.obengkel_application.data.model.User
+import com.example.aplikasiobengkel.data.model.User
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(user: User)
+    suspend fun insert(user: User)
 
-    @Query("SELECT * FROM user WHERE email LIKE :email")
-    fun getUsername(email: String):User
+    @Query("SELECT * FROM user WHERE email = :email")
+    suspend fun getUsername(email: String): User
 }
