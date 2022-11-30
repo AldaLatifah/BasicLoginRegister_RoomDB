cb5c40d2cafaf0ed16bb90142fe8cf05902eb50e
dionpouw <aldion.sumampouw@gmail.com>
2022-11-30 16:00:27 +0800

    change package

--- app/src/main/java/com/example/aplikasiobengkel/ui/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/aplikasiobengkel/ui/auth/register/RegisterViewModel.kt
@@ -3,7 +3,7 @@ package com.example.aplikasiobengkel.ui.auth.register
 import android.app.Application
 import androidx.lifecycle.*
 import com.example.aplikasiobengkel.data.repository.UserRepository
-import com.example.obengkel_application.data.model.User
+import com.example.aplikasiobengkel.data.model.User
 import kotlinx.coroutines.launch
 
 class RegisterViewModel(application: Application) : ViewModel() {
